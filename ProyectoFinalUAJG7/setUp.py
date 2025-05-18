005b2db8cfbf0cb99184276c621629218c69512d
Rafael Argandoña Blacido <rargan01@ucm.es>
2025-05-19 00:10:45 +0200

    Cosas para renderizar video

--- ProyectoFinalUAJG7/setUp.py
+++ ProyectoFinalUAJG7/setUp.py
@@ -20,31 +20,6 @@ def install_model():
     print("Descargando modelo medium...")
     model = whisper.load_model("medium")
     print("Modelo descargado con exito")
-    # Función para instalar un paquete
-def install_package(package):
-    try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package, "-t", os.path.join(env_dir, "Lib", "site-packages")])
-        print(f"{package} instalado correctamente.")
-    except subprocess.CalledProcessError:
-        print(f"Error al instalar {package}.")
-
-def configure_ffmpeg():
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    string_to_remove = "ProyectoFinalUAJG7\\Assets\\SubtitleGenerator\\Python"
-    ffmpeg_path = script_dir.replace(string_to_remove, "")
-    ffmpeg_path = ffmpeg_path + "ffmpeg 7.1.1\\bin"
-    return ffmpeg_path
 
 create_virtualenv()
-# Paquetes necesarios
-packages = [
-    "openai-whisper>=20230314"
-]
-
-### CONFIGURACION DEL ENTORNO ###
-# Instalar todos los paquetes
-for package in packages:
-    install_package(package)
-print("Instalación completa de todos los paquetes.")
-
 install_model()
